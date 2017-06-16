f74f46e4a3222fe0f6e10e38c204d2e3108bbf18
Albert Wang <git@albertyw.com>
2017-06-16 16:54:48 -0700

    Add basic script to get most common committers

---
+++ git_reviewers/reviewers.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+try:
+    import configparser
+except ImportError:
+    raise ImportError("Must be using Python 3")
+
+import argparse
+import os
+import subprocess
+
+
+UBER=True
+
+
+def extract_username(shortlog):
+    shortlog = shortlog.strip()
+    email = shortlog[shortlog.rfind("<")+1:]
+    email = email[:email.find(">")]
+    if UBER:
+        if email[-9:] == '@uber.com':
+            return email[:-9]
+        else:
+            return None
+    return email
+
+
+def get_reviewers():
+    reviewers = []
+    git_shortlog_command = ['git', 'shortlog', '-sne']
+    process = subprocess.run(git_shortlog_command, stdout=subprocess.PIPE)
+    git_shortlog = process.stdout.decode("utf-8").split("\n")
+    reviewers = [extract_username(shortlog) for shortlog in git_shortlog]
+    reviewers = [username for username in reviewers if username]
+    return reviewers
+
+
+def show_reviewers(reviewers):
+    print(", ".join(reviewers))
+
+def main():
+    description = "Suggest reviewers for your diff.\n"
+    description += "https://github.com/albertyw/git-reviewers"
+    parser = argparse.ArgumentParser(description=description)
+    args = parser.parse_args()
+
+    reviewers = get_reviewers()
+    show_reviewers(reviewers)
+
+
+if __name__ == "__main__":
+    main()
