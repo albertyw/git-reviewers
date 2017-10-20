64a7c001c65957ca8c74c0b70e94b26b6ad44238
Albert Wang <git@albertyw.com>
2017-10-19 23:48:54 -0700

    Add version

--- git_reviewers/reviewers.py
+++ git_reviewers/reviewers.py
@@ -4,9 +4,11 @@ import argparse
 import subprocess
 import sys
 
+
 if sys.version_info < (3, 0): # NOQA pragma: no cover
     raise SystemError("Must be using Python 3")
 
+__version__ = '0.1.1'
 UBER = False
 
 
